2df0fb91e295114bd9fe8f9c9f2c8e6b1df27daf
MilesJPool <MilesJPool@gmail.com>
2016-01-15 15:22:35 +0000

    Adds tests around item names.

---
+++ src/GildedRose.Tests/ItemTests.cs
@@ -0,0 +1,15 @@
+﻿using GildedRose.Console;
+using NUnit.Framework;
+
+namespace GildedRose.Tests
+{
+    [TestFixture]
+    class ItemsHaveCorrectNamesTests
+    {
+        [Test]
+        public void AgedBrieHasCorrectName()
+        {
+            Assert.That(new AgedBrie().Name, Is.EqualTo("Aged Brie"));
+        }
+    }
+}
